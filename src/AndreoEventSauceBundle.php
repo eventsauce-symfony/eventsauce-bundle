8a2bea4b730d9a973e97e22dff8c09c41ac6f62f
andreo <andrze.pakula@gmail.com>
2022-01-30 20:30:10 +0100

    Register handle event with headers compiler pass

--- src/AndreoEventSauceBundle.php
+++ src/AndreoEventSauceBundle.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace Andreo\EventSauceBundle;
 
+use Andreo\EventSauce\Messenger\DependencyInjection\HandleEventWithHeadersPass;
+use Symfony\Component\Config\Resource\ClassExistenceResource;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AndreoEventSauceBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addResource(new ClassExistenceResource(HandleEventWithHeadersPass::class));
+        if (class_exists(HandleEventWithHeadersPass::class)) {
+            $container->addCompilerPass(new HandleEventWithHeadersPass());
+        }
+    }
 }
