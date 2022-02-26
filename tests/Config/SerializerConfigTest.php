f6860bdb75bad497f75d722335e6abd0ee4105c5
andreo <andrze.pakula@gmail.com>
2022-02-26 17:13:00 +0100

    Change serializer di

--- tests/Config/SerializerConfigTest.php
+++ tests/Config/SerializerConfigTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Config;
 
+use Andreo\EventSauce\Serialization\SymfonyPayloadSerializer;
 use Andreo\EventSauceBundle\DependencyInjection\AndreoEventSauceExtension;
 use EventSauce\EventSourcing\Serialization\PayloadSerializer;
 use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
@@ -30,5 +31,21 @@ final class SerializerConfigTest extends AbstractExtensionTestCase
         $this->assertContainerBuilderHasAlias(PayloadSerializer::class);
         $payloadSerializerAlias = $this->container->getAlias(PayloadSerializer::class);
         $this->assertEquals(DummyCustomPayloadSerializer::class, $payloadSerializerAlias->__toString());
+    }
+
+    /**
+     * @test
+     */
+    public function should_register_symfony_payload_serializer(): void
+    {
+        $this->load([
+            'payload_serializer' => SymfonyPayloadSerializer::class,
+        ]);
+
+        $this->assertContainerBuilderHasAlias(PayloadSerializer::class);
+        $payloadSerializerAlias = $this->container->getAlias(PayloadSerializer::class);
+        $this->assertEquals(SymfonyPayloadSerializer::class, $payloadSerializerAlias->__toString());
+        $this->assertContainerBuilderHasService('andreo.event_sauce.symfony_serializer');
+        $this->container->getDefinition('andreo.event_sauce.symfony_serializer');
     }
 }
