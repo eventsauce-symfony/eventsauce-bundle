1e374acb94ffcb258cbfebeb1fcd03822240298e
andreo <andrze.pakula@gmail.com>
2022-05-10 20:50:36 +0200

    Rename config root node

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -40,7 +40,7 @@ final class Configuration implements ConfigurationInterface
 
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('andreo_eventsauce');
+        $treeBuilder = new TreeBuilder('andreo_event_sauce');
         /** @var ArrayNodeDefinition $rootNode */
         $rootNode = $treeBuilder->getRootNode();
 
