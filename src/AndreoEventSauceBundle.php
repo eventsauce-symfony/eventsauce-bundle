8a760d29a3eecfc4f9e9c1600603b81fffcb6458
andreo <andrze.pakula@gmail.com>
2022-05-21 20:20:10 +0200

    Add resource before check compiler pass class

--- src/AndreoEventSauceBundle.php
+++ src/AndreoEventSauceBundle.php
@@ -7,6 +7,7 @@ namespace Andreo\EventSauceBundle;
 use Andreo\EventSauce\Messenger\DependencyInjection\HandleEventSauceMessageMiddlewarePass;
 use Andreo\EventSauceBundle\DependencyInjection\CompilerPass\AclInboundPass;
 use Andreo\EventSauceBundle\DependencyInjection\CompilerPass\AclOutboundPass;
+use Symfony\Component\Config\Resource\ClassExistenceResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -16,6 +17,8 @@ final class AndreoEventSauceBundle extends Bundle
     {
         $container->addCompilerPass(new AclOutboundPass('andreo.eventsauce.acl_outbound_enabled'), priority: -10);
         $container->addCompilerPass(new AclInboundPass('andreo.eventsauce.acl_inbound_enabled'), priority: -10);
+
+        $container->addResource(new ClassExistenceResource(HandleEventSauceMessageMiddlewarePass::class));
         if (class_exists(HandleEventSauceMessageMiddlewarePass::class)) {
             $container->addCompilerPass(new HandleEventSauceMessageMiddlewarePass('andreo.eventsauce.messenger_enabled'), priority: -15);
         }
